294fda3420a63945c0134cce90668811d456322f
Семен Ильин <ifkfvjif@gmail.com>
2025-07-16 00:57:51 +0500

    DockerSetup: скачивание nuget-ов

---
+++ LinkPulse.Api/Entities/ShortenedUrl.cs
@@ -0,0 +1,12 @@
+namespace LinkPulse.Api.Entities;
+
+public class ShortenedUrl
+{
+    public Guid Id { get; set; }
+    public string LongUrl { get; set; }
+    public string  ShortCode { get; set; }
+    public DateTime CreatedOnUtc { get; set; }
+    
+    public int ClickCount { get; set; }
+    
+}
