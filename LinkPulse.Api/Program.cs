76fa3830cb2c29d8a4232eeb118392f2e8f34c93
Семен Ильин <ifkfvjif@gmail.com>
2025-07-21 01:55:35 +0500

    Настройка под .net 8

--- LinkPulse.Api/Program.cs
+++ LinkPulse.Api/Program.cs
@@ -3,9 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
@@ -14,8 +13,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-}
+    app.UseSwagger();
+    app.UseSwaggerUI();}
 
 app.UseHttpsRedirection();
 app.Run();
