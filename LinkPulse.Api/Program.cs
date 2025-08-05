b608cfe73efbe8d940c56b4e2e89a0ad0dbf1a7d
Семен Ильин <90569031+Kibatun@users.noreply.github.com>
2025-08-05 13:28:21 +0500

    Донастройка ручки и обработки данных

--- LinkPulse.Api/Program.cs
+++ LinkPulse.Api/Program.cs
@@ -9,14 +9,21 @@ builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
 var app = builder.Build();
+
+//Автоматическая миграция при каждом запуске
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+//
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    
 }
-
 app.MapControllers();
 app.Run();
